7985221025f652b593b49a2c347ffe97081eb952
Ahmadou Dicko <mail@ahmadoudicko.com>
2021-08-29 21:05:25 +0000

    Logic for palette and remove all rmarkdown templates

---
+++ R/display_unhcr_pal.R
@@ -0,0 +1,78 @@
+#' UNHCR color palettes viewer
+#'
+#' Displays a set of the color palettes from UNHCRcolor
+#'
+#' @param n Number of different colors in the palette, minimum depending on the palette (2), maximum depending on the palette (5, 7 or 10)
+#' @param type Type of the palette, can be "sequential", "diverging", "qualitative" or "all"
+#'
+#' @importFrom graphics par plot rect text
+#'
+#' @examples
+#' library(unhcrthemes)
+#' display_unhcr_all()
+#' display_unhcr_all(type = "diverging")
+#' display_unhcr_all(type = c("diverging", "qualitative", "sequential"))
+#' display_unhcr_all(3, type = "sequential")
+#' display_unhcr_all(5, type = "sequential")
+#'
+#' @export
+display_unhcr_all <- function(n = NULL, type = "all"){
+  if(any(type == "all")){
+    selected_type <- unhcrthemes::unhcrcolors
+  } else if (any(type %in% c("sequential", "diverging", "qualitative"))){
+    selected_type = unhcrthemes::unhcrcolors[unhcrthemes::unhcrcolors$type %in% type, ]
+  } else{
+    stop(paste(type, "is not a valid name for a color type\n"))
+  }
+  selected_metadata <- unhcrthemes::unhcrcolors[unhcrthemes::unhcrcolors$name %in% selected_type$name, ]
+
+  n_colors <- nrow(selected_metadata)
+
+  if(is.null(n)){
+    my_n <- selected_metadata$max_n
+  } else{
+    my_n <- rep(n, n_colors)
+  }
+
+  selected_colors <- vector("list", n_colors)
+
+  ylim <- c(0, n_colors)
+  oldpar <- par(mgp = c(2, 0.25, 0))
+  on.exit(par(oldpar))
+  plot(1, 1, xlim = c(0, max(my_n)), ylim = ylim,
+       type = "n", axes = FALSE, bty = "n", xlab = "", ylab = "")
+
+  for(i in seq_len(n_colors)) {
+    one_color <- unhcr_pal(n = my_n[i],
+                           name = selected_metadata$name[i])
+    rect(xleft = 0:(my_n[i] - 1) + 0.2,
+         ybottom = i - 1,
+         xright = 1:my_n[i] + 0.2,
+         ytop = i-0.2,
+         col = one_color,
+         border = "light grey")
+  }
+  text(rep(0.2, n_colors), (1:n_colors) - 0.6,
+       labels = selected_metadata$name, xpd = TRUE, adj = 1)
+}
+
+#' UNHCR color palette viewer
+#'
+#' Displays a color palette from UNHCR brand color
+#'
+#' @param n Number of different colors in the palette, minimum depending on palette (2), maximum depending on palette (5, 7 or 10)
+#' @param name A palette name
+#'
+#' @importFrom graphics image
+#'
+#' @examples
+#' display_unhcr_pal(7, "pal_main_c")
+#'
+#' @export
+display_unhcr_pal <- function(n = NULL, name){
+        selected_colors <- unhcr_pal(n, name)
+        selected_type <- unhcrthemes::unhcrcolors[unhcrthemes::unhcrcolors$name == name, ][["type"]][[1]]
+        image(1:n, 1, as.matrix(1:n), col = selected_colors,
+              main = paste0(name, ": ", selected_type, " (", n ,")"),
+              xlab = " ", ylab = " ", xaxt = "n", yaxt = "n", bty="n")
+}
---
+++ R/palettes.R
@@ -0,0 +1,52 @@
+#' UNHCR color palettes
+#'
+#' Creates a color palette from UNHCR branding
+#'
+#' @param n Number of different colors in the palette, minimum depending on palette (2), maximum depending on palette (5, 7 or 10)
+#' @param name A palette name
+#' @param ... Additional arguments for [grDevices::colorRampPalette]
+#'
+#' @return A character vector
+#'
+#' @examples
+#' unhcr_pal(3, "pal_main_c")
+#' unhcr_pal(3, "pal_blue_s")
+#' unhcr_pal(5, "pal_yellow_s")
+#' unhcr_pal(12, "pal_red_s")
+#' @export
+unhcr_pal = function(n = NULL, name, ...){
+  if (!(name %in% unhcrthemes::unhcrcolors$name)){
+    stop(paste(name,"is not a valid palette name\n"),
+         call. = FALSE)
+  }
+  selected_metadata <- unhcrthemes::unhcrcolors[unhcrthemes::unhcrcolors$name == name, ]
+  min_n <- selected_metadata$min_n
+  max_n <- selected_metadata$max_n
+  type <- selected_metadata$type
+  if(is.null(n)) {
+    n <- max_n
+  }
+  proper_n <- n
+  if(proper_n > max_n) {
+    proper_n <- max_n
+  }
+  if(!(n %in% min_n:max_n) && type == "qualitative") {
+    warning(paste("Number of colors (n) in the", name,
+                  "palette should be between", min_n, "and", max_n,
+                  "\n"), call. = FALSE)
+    n <- proper_n
+  }
+  if(!(n >= min_n)) {
+    warning(paste("Number of colors (n) in the", name,
+                  "palette should be between", min_n, "and",
+                  max_n, "\n"), call. = FALSE)
+    proper_n = min_n
+    n <- min_n
+  }
+  coln <- paste0("n", proper_n)
+  colors <- unhcrthemes::unhcrcolors[unhcrthemes::unhcrcolors$name == name, ][[coln]][[1]]
+  if (n > 2) {
+    colors <- grDevices::colorRampPalette(colors, ...)(n = n)
+  }
+  return(colors)
+}
