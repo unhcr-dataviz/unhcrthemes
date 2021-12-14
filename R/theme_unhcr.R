206174f8f573e5275cff8bfd82034b6df05c5744
Ahmadou Dicko <mail@ahmadoudicko.com>
2021-12-14 21:32:16 +0000

    Tentative fix for subtitle and robust class name

--- R/theme_unhcr.R
+++ R/theme_unhcr.R
@@ -209,6 +209,7 @@ theme_unhcr <- function(font_size = 9, font_family = "Lato", line_size = .5,
   ret <- ret + ggplot2::theme(plot.subtitle = ggplot2::element_text(
     size = font_size, color = dark_grey, face = "plain",
     hjust = 0, vjust = 1,
+    ## margin = ggplot2::margin(t = -half_line, b = font_size * rel_large)
     margin = ggplot2::margin(t = -half_line, b = font_size * rel_large)
   ))
   ret <- ret + ggplot2::theme(plot.title.position = "plot")
@@ -233,22 +234,26 @@ theme_unhcr <- function(font_size = 9, font_family = "Lato", line_size = .5,
   ret <- ret + ggplot2::theme(plot.margin = ggplot2::margin(font_size, font_size, font_size, font_size))
 
   # class for condition on tag
-  class(ret) <- c("conditional_theme", class(ret))
+  class(ret) <- c("conditional_unhcr_theme", class(ret))
   attr(ret, "font_size") <- font_size
+  attr(ret, "rel_large") <- rel_large
   ret
 }
 
-#' @importFrom ggplot2 ggplot_add
+#' @importFrom ggplot2 ggplot_add element_text margin
 #' @noRd
 #' @export
-ggplot_add.conditional_theme <- function(object, plot, object_name) {
+ggplot_add.conditional_unhcr_theme <- function(object, plot, object_name) {
   # TODO Find a way to get margin based on font_size from the main theme.
   # It's called twice to have the font_size
   plot$theme <- ggplot2:::add_theme(plot$theme, object, object_name)
+  font_size <- plot$theme$text$size
+  rel_large <- attr(plot$theme, "rel_large")
   if (!is.null(plot$labels$tag)) {
-    object <- object + ggplot2::theme(plot.title = ggplot2::element_text(
-      margin = ggplot2::margin(t = plot$theme$text$size * 1.5, b = 4.5)
-    ))
+    object <- object +
+      ggplot2::theme(plot.title = ggplot2::element_text(
+        margin = ggplot2::margin(t = font_size * 1.5, b = font_size * 0.5)),
+        plot.subtitle = ggplot2::element_text(margin = ggplot2::margin(t = -font_size * 0.075, b = rel_large * font_size)))
   }
   plot$theme <- ggplot2:::add_theme(plot$theme, object, object_name)
   plot
