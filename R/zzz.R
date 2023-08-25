0011ea9d594710f8f96519bb8f24707ea0f98e44
Cedric Vidonne <cedric@vidonne.me>
2023-08-25 09:18:57 +0200

    fix font warning

--- R/zzz.R
+++ R/zzz.R
@@ -4,15 +4,19 @@
 
   sys_fonts <- systemfonts::system_fonts()
   if (!any(grepl("lato", sys_fonts$family, ignore.case = TRUE))) {
-    warnings("Lato font is missing, please install it!",
-             call. = FALSE)
-    systemfonts::register_font(name = "Lato",
-                               plain = file.path(font_dir, "Lato-Regular.ttf"),
-                               italic = file.path(font_dir, "Lato-Italic.ttf"),
-                               bold = file.path(font_dir, "Lato-Bold.ttf"),
-                               bolditalic = file.path(font_dir,
-                                                      "Lato-BoldItalic.ttf"))
-
+    warning("Lato font is missing, please install it!",
+      call. = FALSE
+    )
+    systemfonts::register_font(
+      name = "Lato",
+      plain = file.path(font_dir, "Lato-Regular.ttf"),
+      italic = file.path(font_dir, "Lato-Italic.ttf"),
+      bold = file.path(font_dir, "Lato-Bold.ttf"),
+      bolditalic = file.path(
+        font_dir,
+        "Lato-BoldItalic.ttf"
+      )
+    )
   }
   update_geom_font_defaults()
 }
